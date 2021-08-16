ed0e179de5661614082dccad27ddbbb4c7a21686
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-08-16 10:59:02 -0600

    done with general CRUD ops

--- src/main/java/com/codeup/blogapp/web/PostController.java
+++ src/main/java/com/codeup/blogapp/web/PostController.java
@@ -15,16 +15,16 @@ public class PostController {
     @GetMapping
     private List<Post> getPost() { // get BLOG Posts not Post request
         List<Post> posts = new ArrayList<>();
-        posts.add(new Post(69L, "test1", "fdsfdsfafds"));
-        posts.add(new Post(420L, "test2", "fdsfdsfafdsfdsfdsfafdsfdsfdsfafdsfdsfdsfafds"));
-        posts.add(new Post(9L, "test3", "fdsfdsfafds fdsfdsfafds fdsfdsfafds"));
+        posts.add(new Post(69L, "Clifford Joins the Army", "War, war never changes"));
+        posts.add(new Post(420L, "Clifford Invests In Cryptocurrency", "He lost alot of money on DogeCoin"));
+        posts.add(new Post(9L, "Clifford Finds Infinity Gauntlet", "Reality can be anything I want *SNAPS* infinite dog treats"));
 
         return posts;
     }
 
     //Ex. someone wants to make a get request with specific constraints (user only wants one post back based on ID
     //Ex. someone is making a request to like "/api/posts/1 - with 1 being ID
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     private Post getPostByID(@PathVariable Long id) {
         if (id == 1) {
             return new Post(69L, "test1", "fdsfdsfafds");
@@ -42,14 +42,14 @@ public class PostController {
         System.out.println(newPost.getContent());
     }
 
-    @PutMapping("{id}")
+    @PutMapping("/{id}")
     private void updatePost(@PathVariable Long id, @RequestBody Post updatedPost) {
         System.out.println(updatedPost.getId());
         System.out.println(updatedPost.getTitle());
         System.out.println(updatedPost.getContent());
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("/{id}")
     private void deletePost(@PathVariable Long id) {
         System.out.println("You successfully deleted post with ID: " + id);
     }
--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -7,18 +7,21 @@ export default function PostIndex(props) {
             <h1>Posts Page</h1>
         </header>
         <main>
-        	<form id="post-form">
-        		<input name="newPost-id" id="newPost-id" type="text" placeholder="ID"><br>
-        		<input name="newPost-title" id="newPost-title" type="text" placeholder="Title"><br>
-        		<textarea name="newPost-content" id="newPost-content" cols="30" rows="10" placeholder="[Enter Post Content Here]"></textarea><br>
-        		<button id="newPost-submit" type="button"> Submit </button>
+        	<form id="post-form" class="row">
+        		<input class="col-3" name="newPost-id" id="newPost-id" data-id="newPost-id" type="text" placeholder="ID"><br>
+        		<input class="col-3" name="newPost-title" id="newPost-title" type="text" placeholder="Title"><br>
+        		<textarea class="col-9" name="newPost-content" id="newPost-content" cols="50" rows="5" placeholder="[Enter Post Content Here]"></textarea><br>
+        		<button class="col-3" id="newPost-submit" type="button"> Submit </button>
 			</form>
 <!--         Make a  form here, forms from HTML amd will have a click event and listen to that id, the event fires off, now  -->
             <div>
                 ${props.posts.map(post => `
-					<h3>${post.title}</h3>
-					<h5>${post.content}</h5>
-					<div><br></div>
+					<div class="post">
+						<h3 class="edit-title" contenteditable="false">${post.title}</h3>
+						<h6 class="edit-body" contenteditable="false">${post.content}</h6>
+						<button type="button" data-id=${post.id} class="editPost-btn" >Edit</button>
+						<button type="button" class="deletePost-btn" data-id=${post.id}>Delete</button>
+					</div>
 				<!-- Add Edit/Delete Button -->
 				`).join('')}   
             </div>
@@ -28,10 +31,11 @@ export default function PostIndex(props) {
 
 export function PostEvents() {
 	newPostSubmitClickEvent();
+	postEditClickEvent();
+	postDeleteClickEvent();
 }
 
-function newPostSubmitClickEvent(){
-
+function newPostSubmitClickEvent() {
 	$("#newPost-submit").click(function () {
 		let url = "http://localhost:8080/api/posts"
 		let request = {
@@ -54,10 +58,71 @@ function newPostSubmitClickEvent(){
 
 }
 
-function postEditClickEvent (){
+function postEditClickEvent() {
+	$('.editPost-btn').click(function () {
+		let editable = $(this).siblings('.edit-title').attr("contenteditable");
+		if ( editable === "false" ) {
+			$(this).siblings(".edit-title, .edit-body").attr("contenteditable", true).css({
+				'border': "solid 1px black"
+			})
+			$(this).text('Save')
+
+		} else {
+
+			$(this).siblings(".edit-title, .edit-body").attr("contenteditable", false)
+			let request = {
+				'id':$(this).attr('data-id'),
+				'title':$(this).siblings('.edit-title').text(),
+				'content': $(this).siblings('.edit-body').text()
+			}
+			submitEditEvent(request)
+			$(this).text('Edit')
+		}
+
+	})
 }
 
-function postDeleteClickEvent(){
+function submitEditEvent(requestBody) {
+	fetch(`http://localhost:8080/api/posts/${requestBody.id}`, {
+		method: "PUT",
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(requestBody)
+	}).then(response => {
+		console.log(response)
+		createView("/posts")
+	}).catch(error => {
+		console.log(error)
+	})
+}
+
+function postDeleteClickEvent() {
+	$('.deletePost-btn').click(function () {
+		if( confirm("are you sure?") ){
+			let id = $(this).attr('data-id')
+			submitDeleteEvent(id)
+		}
+	});
+}
+
+function submitDeleteEvent(id){
+	fetch(`http://localhost:8080/api/posts/${id}`, {
+		method: "DELETE",
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then(response => {
+		console.log(response)
+		createView("/posts")
+	}).catch(error => {
+		console.log(error)
+	})
+}
+
+function fetchRequest(body='', id){
 
 }
+
+
 
