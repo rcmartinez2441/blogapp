aaae2dc7758d8f79fed4936923049580036b6e16
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-08-18 22:45:15 -0600

    working on more styling and User functionality

--- src/main/resources/static/js/views/User.js
+++ src/main/resources/static/js/views/User.js
@@ -3,13 +3,8 @@ import {postDeleteClickEvent, postEditClickEvent} from "./PostIndex.js";
 
 export default function User(props){
 	return `
-		<!DOCTYPE html>
-		<html lang="en">
-			<head>
-				<meta charset="UTF-8"/>
-				<title>Search</title>
-			</head>
-			<body>
+			<div class="container">
+				<h2>Search User</h2>
 				<div class="searchBar">
 					<input id="search" name="search" type="text" placeholder="Search Here">
 					<select name="findBy" id="findBy">
@@ -18,10 +13,9 @@ export default function User(props){
 					</select>
 					<button id="searchBtn" type="button">Search</button>
 				</div>
-				<div id="userResult">
+				<div id="userResult" class="mx-auto">
 				</div>
-			</body>
-		</html>	
+			</div>
 	`
 }
 
@@ -67,18 +61,20 @@ function displayResultsInDOM(data){
 	let userResult = $('#userResult')
 	userResult.html('');
 	userResult.append(`
-		<h2>Welcome Back, ${data.username}</h2>
-		<div><a href="#" id="updatePassword">Update Password</a></div>
+		<div class="main-body">
+			<h2 class="my-4">Welcome Back, <span style="color:#7FC8A9">${data.username}</span></h2>
+			<div><a href="#" class="mb-5" id="updatePassword">Update Password</a></div>
+		</div>
 	`);
 
 	data.posts.forEach(post => {
 		userResult.append(`
-			<div class="post">
-				<h3 class="edit-title" contenteditable="false">${post.title}</h3>
-				<h6 class="edit-body" contenteditable="false">${post.content}</h6>
+			<div class="post row row-cols-1 border rounded mb-2 shadow p-2">
+				<h3 class="edit-title col" contenteditable="false">${post.title}</h3>
+				<h6 class="edit-body col" contenteditable="false">${post.content}</h6>
 				<div><em>Created By: ${data.username}</em></div>
-				<button type="button" data-id=${post.id} class="editPost-btn" >Edit</button>
-				<button type="button" class="deletePost-btn" data-id=${post.id}>Delete</button>
+				<button type="button" data-id=${post.id} class="editPost-btn btn btn-info col-2 m-1" >Edit</button>
+				<button type="button" class="deletePost-btn btn btn-info col-2 m-1" data-id=${post.id}>Delete</button>
 			</div>
 		`)
 	})
@@ -89,8 +85,21 @@ function displayResultsInDOM(data){
 }
 
 function updatePasswordEvent(){
-	$('#updatePassword').click(function (){
-		console.log('Click Event Works')
+	$('#updatePassword').one("click", function (){
+		$(this).parent().append(`
+			<div class="password-section mt-1">
+				<label for="old-password">Old Password</label><br>
+				<input class="mb-4" name="old-password" placeholder="Enter Old Password">
+				<br>
+				<label for="new-password">New Password</label><br>
+				<input class="mb-4" name="new-password" placeholder="Enter New Password">
+				<br>
+				<label for="new-password2">Re-Enter New Password</label><br>
+				<input class="mb-4" name="new-password2" placeholder="Enter New Password">
+				<br>
+				<button class="" id="submitPassword">Confirm</button>
+			</div>
+		`)
 	})
 }
 
--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -12,22 +12,22 @@ export default function Navbar(props) {
         <a class="navbar-brand p-1" href="#">BlogApp</a>
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a href="/" data-link class="p-1" style="text-decoration:none; color:white">Home</a>
+                <a href="/" data-link class="p-2" style="text-decoration:none; color:white">Home</a>
             </li>
             <li class="nav-item active">
-                <a href="/posts" data-link class="p-1" style="text-decoration:none; color:white">Posts</a>
+                <a href="/posts" data-link class="p-2" style="text-decoration:none; color:white">Posts</a>
             </li>
             <li class="nav-item">
-                <a href="/about" data-link class="p-1" style="text-decoration:none; color:white">About</a>
+                <a href="/about" data-link class="p-2" style="text-decoration:none; color:white">About</a>
             </li>
             <li class="nav-item">
-                <a href="/login" data-link class="p-1" style="text-decoration:none; color:white">Login</a>
+                <a href="/login" data-link class="p-2" style="text-decoration:none; color:white">Login</a>
             </li>
             <li class="nav-item">
-                <a href="/users" data-link class="p-1" style="text-decoration:none; color:white">Register</a>
+                <a href="/users" data-link class="p-2" style="text-decoration:none; color:white">Register</a>
             </li>
             <li class="nav-item">
-                <a href="/search" data-link class="pr-1" style="text-decoration:none; color:white">Search</a>
+                <a href="/search" data-link class="p-2" style="text-decoration:none; color:white">Search</a>
             </li>
         </ul>
 </nav>
