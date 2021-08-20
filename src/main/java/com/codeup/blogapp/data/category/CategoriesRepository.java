74a71de3ffeeb66ac5a4d2503d747101df903107
rcmartinez2441 <rcmartinez2441@gmail.com>
2021-08-20 15:47:56 -0600

    working with more data repos:

---
+++ src/main/java/com/codeup/blogapp/data/category/CategoriesRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.blogapp.data.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriesRepository  extends JpaRepository<Category, Long> {
+    Category findFirstByName(String name);
+}
--- src/main/java/com/codeup/blogapp/web/CategoriesController.java
+++ src/main/java/com/codeup/blogapp/web/CategoriesController.java
@@ -1,6 +1,7 @@
 package com.codeup.blogapp.web;
 
 
+import com.codeup.blogapp.data.category.CategoriesRepository;
 import com.codeup.blogapp.data.category.Category;
 import com.codeup.blogapp.data.post.Post;
 import com.codeup.blogapp.data.user.User;
@@ -13,45 +14,20 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/categories", headers = "Accept=application/json")
 public class CategoriesController {
-    List<Category> sampleCategories = new ArrayList<Category>();
-    Collection<Post> samplePosts = new ArrayList<Post>();
 
-    CategoriesController() {
-        //Loops to add 5 categories and posts to each respective variable
-        for (long i = 1; i < 6; i++) {
-            samplePosts.add(new Post(i,"Title" + i, "Content" + i, new User("user1")));
-            sampleCategories.add(new Category(i, "category" + i));
-        }
+    private CategoriesRepository categoriesRepository;
 
-        //Loops up until the total amount of Categories then inserts a random number of posts in samplePosts per category
-        for (int i = 0; i < sampleCategories.size(); i++) {
-            int numOfPostsToAdd = (int) Math.floor((Math.random() * 5) + 1);
-            Collection<Post> randomPosts = new ArrayList<>();
-            for (Post post : samplePosts) {
-                if ( (post.getId() == numOfPostsToAdd) && (post.getId() > 1) ) {
-                    break;
-                } else{
-                    randomPosts.add(post);
-                }
-            }
-            sampleCategories.get(i).setPosts(randomPosts);
-        }
-        System.out.println(samplePosts);
+    public CategoriesController(CategoriesRepository categoriesRepository) {
+        this.categoriesRepository = categoriesRepository;
     }
 
     @GetMapping
     private List<Category> getCategories (){
-        return sampleCategories;
+        return categoriesRepository.findAll();
     }
 
     @GetMapping("/{name}")
     private Category getPostsByCategory(@PathVariable String name) {
-        for (Category thisCategory: sampleCategories){
-            if (thisCategory.getName().equals(name)){
-                return thisCategory;
-            }
-        }
-
-        return null;
+        return categoriesRepository.findFirstByName(name);
     }
 }
